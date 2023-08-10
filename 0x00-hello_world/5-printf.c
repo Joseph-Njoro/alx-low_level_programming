640960fdf0272274c370603e593cb4686675802f
Joseph-Njoro <ramosnjoro@gmail.com>
2023-08-10 08:48:22 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * initializing  main function
  * using printf instead of printf
  **/
-int main()
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
