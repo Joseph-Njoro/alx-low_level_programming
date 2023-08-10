0b492945e0708ac29a5a6f1451a7873cff068599
Joseph-Njoro <ramosnjoro@gmail.com>
2023-08-10 09:07:18 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
  *main entry point
- * Description: prints a text using printf
- * Return: Always 0 
+ *Description: prints a text using printf
+ *Return: Always 0 
  **/
 int main(void)
 {
