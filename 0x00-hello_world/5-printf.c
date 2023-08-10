364c200ba080ac1617245ce9477b8819627304fb
Joseph-Njoro <ramosnjoro@gmail.com>
2023-08-10 08:46:57 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * initializing  main function
+ * using printf instead of printf
+ **/
 int main()
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return 0;
 }
