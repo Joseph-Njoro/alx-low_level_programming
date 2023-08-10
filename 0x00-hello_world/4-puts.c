2532678d16a7b1c6d6d5441135a358b7e60c9d86
Joseph-Njoro <ramosnjoro@gmail.com>
2023-08-10 08:32:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-int main()
+int main(void)
 {
-    printf("Programming is like building a multilingual puzzle\n");
+    puts("Programming is like building a multilingual puzzle");
     return 0;
 }
