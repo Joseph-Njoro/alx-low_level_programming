b9e4b86e9558c92a9dfd29457cb4bdc5e3cde52c
Joseph-Njoro <ramosnjoro@gmail.com>
2023-08-16 23:49:46 -0700

    trail 4 largest no

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,22 +11,17 @@
 int largest_number(int a, int b, int c)
 {
     int largest;
-    if(a > b && a > c)
-    {
-        largest = a;
-    }
-    else if(a > b && c > a)
-    {
-        largest = c;
-    }
-    else if (b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest - c;
-    }
+    if (a >= b && a >= c)
+{
+    largest = a;
+}
+else if (b >= a && b >= c)
+{
+    largest = b;
+}
+else if (c >= a && c >= b)
+{
+    largest = c;
+}
     return(largest);
 }
-
