52b4893da607bc25c5bf840b75316c783f9afed1
Joseph-Njoro <ramosnjoro@gmail.com>
2023-08-17 00:07:29 -0700

    trial 8 largest no

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void largest_number(int a, int b, int c);
+int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
