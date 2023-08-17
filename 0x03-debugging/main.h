e097ed22b9c89af8696ee7738d6617cd17ce50ec
Joseph-Njoro <ramosnjoro@gmail.com>
2023-08-16 23:56:30 -0700

    trail 6 largest no

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void largest_number(int a, int b, int c)
+void largest_number(int a, int b, int c);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
