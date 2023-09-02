76f0acdb1cb3396b91209d0ed6226aaef9f93559
Joseph-Njoro <ramosnjoro@gmail.com>
2023-09-02 04:43:39 -0700

    static libraries

---
+++ 0x09-static_libraries/compile_sourcefiles_to_objectfiles.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#compiles souce files into object codes
+
+# List of source files to compile
+source_files=(
+    0-isupper.c
+    0-memset.c
+    0-strcat.c
+    100-atoi.c
+    1-isdigit.c
+    1-memcpy.c
+    1-strncat.c
+    2-strlen.c
+    2-strchr.c
+    2-strncpy.c
+    3-islower.c
+    3-puts.c
+    3-strcmp.c
+    3-strspn.c
+    4-isalpha.c
+    4-strpbrk.c
+    5-strstr.c
+    6-abs.c
+    9-strcpy.c
+    main.c
+    _putchar.c
+)
+
+# Loop through the source files and compile each into an object file
+for file in "${source_files[@]}"; do
+    object_file="${file%.c}.o"  # Calculate the object file name
+    gcc -c "$file" -o "$object_file"
+done
+
+# Inform the user that the compilation is complete
+echo "Compilation of source files into object files completed."
