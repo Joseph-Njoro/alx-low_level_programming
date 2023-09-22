ce985730012305b7e8d76a719c889c223e452b43
Joseph-Njoro <ramosnjoro@gmail.com>
2023-09-22 13:06:29 -0700

    2 trial

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
-
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer to a pointer to the head of the list.
- * @str: The string to be duplicated and added to the new node.
- *
- * Return: The address of the new element, or NULL if it failed.
+ * add_node - This is a function will add
+ * a new node at the beginning of a list_t list.
+ * @head: This is the head of the linked list.
+ * @str: This is a string to store in the list.
+ * Return: address of the head is returned.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    if (head == NULL || str == NULL)
-        return (NULL);
+	list_t *new_node;
+	size_t len;
 
-    list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str); // Duplicate the string
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
 
-    new_node->len = strlen(str); // Get the length of the string
-    new_node->next = *head;     // Set the next pointer to the current head
+	len = 0;
 
-    *head = new_node; // Update the head to point to the new node
+	while (str[len])
+		len++;
 
-    return (new_node);
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
 }
