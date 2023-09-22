783a01a151c5796bb7e1116e34eea7716c7b71d9
Joseph-Njoro <ramosnjoro@gmail.com>
2023-09-22 13:03:36 -0700

    2 trial

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,35 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
- * add_node - function that adds a new node at the beginning of list.
- * @head: pointer to singly linked list.
- * @str: pointer to signly linked list.
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be duplicated and added to the new node.
  *
- * str needs to be duplicated.
- * You are allowed to use strdup.
- *
- * Return: The address of the new element or NULL if it failed.
- *
+ * Return: The address of the new element, or NULL if it failed.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	size_t length = 0;
+    if (head == NULL || str == NULL)
+        return (NULL);
 
-	new_node = malloc(sizeof(list_t));
-	/* if it fails returb NULL */
-	if (new_node == NULL)
-		return (NULL);
-	/* loop through the string to find length */
-	while (str[length])
-		length++;
-	/* access the length of new_node and assign it to length */
-	new_node->len = length;
-	/* access the list of new_node and duplicate it */
-	new_node->str = strdup(str);
-	/* access the next node of new_node and assign it as the first node */
-	new_node->next = *head;
-	/* make it the beginning of the list */
-	*head = new_node;
-	/* return it */
-	return (new_node);
+    list_t *new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->str = strdup(str); // Duplicate the string
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->len = strlen(str); // Get the length of the string
+    new_node->next = *head;     // Set the next pointer to the current head
+
+    *head = new_node; // Update the head to point to the new node
+
+    return (new_node);
 }
